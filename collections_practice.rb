9cd97587ad6296af82c1d46056459e2adadbd686
David Anderson <matandersonlv@hotmail.com>
2018-12-02 19:48:02 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
 
+def sort_array_asc(array)
+  # [25, 7, 1]
+
+  array.sort
+
+  # [1,7,25]
+end
+
+def sort_array_desc(array)
+  # [25,7,14]
+  # array.sort
+#   [14, 7, 25]
+  array.sort.reverse
+
+  # [25,14,7]
+end
+
+def sort_array_char_count()
+
+end
